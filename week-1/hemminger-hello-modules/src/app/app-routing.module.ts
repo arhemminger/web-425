929c669f3ae7a4364a175291916ea96eede76e3c
Andrew Hemminger <arhemminger@gmail.com>
2019-07-14 17:26:41 -0500

    adding code attribution

--- week-1/hemminger-hello-modules/src/app/app-routing.module.ts
+++ week-1/hemminger-hello-modules/src/app/app-routing.module.ts
@@ -1,3 +1,12 @@
+/*
+============================================
+; Title:  Modules
+; Author: Professor Krasso
+; Date:  9 July 2019
+; Modified by: Andrew Hemminger
+; Description: Assignment 1.5 - Modules
+;===========================================
+*/
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
