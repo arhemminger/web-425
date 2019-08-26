409abcc1f31c4c5652aa7d5dd506109c2767dc81
Andrew Hemminger <arhemminger@gmail.com>
2019-08-25 20:42:58 -0500

    Code fix and style changes

--- week-7/hemminger-form-validation/src/app/app.component.ts
+++ week-7/hemminger-form-validation/src/app/app.component.ts
@@ -11,7 +11,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'hemminger-form-validation';
